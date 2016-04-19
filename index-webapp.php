7722b0ca88a5836cbca38e32d271e2f0b65c381f
Dan Greene <dgreene@utk.edu>
2016-04-19 15:46:37 -0400

    Add Libraries  footer include and Libraries header info

---
+++ index-webapp.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7 old-ie" lang="en-US">
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8 old-ie" lang="en-US">
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html >
+<!--<![endif]-->
+
+
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
+	<title>University Libraries | The University of Tennessee, Knoxville</title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="shortcut icon" href="https://www.lib.utk.edu/template/2016/images/interface/icon-114x114.png">
+<link rel="shortcut icon" href="https://www.lib.utk.edu/template/2016/images/interface/favicon.gif">
+<link rel="shortcut icon" href="https://www.lib.utk.edu/template/2016/images/interface/favicon.ico">
+
+
+
+
+		<script>
+
+			/*! grunt-grunticon Stylesheet Loader - v2.1.2 | https://github.com/filamentgroup/grunticon | (c) 2015 Scott Jehl, Filament Group, Inc. | MIT license. */
+
+      (function(e){function t(t,n,r,o){"use strict";function a(){for(var e,n=0;u.length>n;n++)u[n].href&&u[n].href.indexOf(t)>-1&&(e=!0);e?i.media=r||"all":setTimeout(a)}var i=e.document.createElement("link"),l=n||e.document.getElementsByTagName("script")[0],u=e.document.styleSheets;return i.rel="stylesheet",i.href=t,i.media="only x",i.onload=o||null,l.parentNode.insertBefore(i,l),a(),i}var n=function(r,o){"use strict";if(r&&3===r.length){var a=e.navigator,i=e.Image,l=!(!document.createElementNS||!document.createElementNS("http://www.w3.org/2000/svg","svg").createSVGRect||!document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image","1.1")||e.opera&&-1===a.userAgent.indexOf("Chrome")||-1!==a.userAgent.indexOf("Series40")),u=new i;u.onerror=function(){n.method="png",n.href=r[2],t(r[2])},u.onload=function(){var e=1===u.width&&1===u.height,a=r[e&&l?0:e?1:2];n.method=e&&l?"svg":e?"datapng":"png",n.href=a,t(a,null,null,o)},u.src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==",document.documentElement.className+=" grunticon"}};n.loadCSS=t,e.grunticon=n})(this);(function(e,t){"use strict";var n=t.document,r="grunticon:",o=function(e){if(n.attachEvent?"complete"===n.readyState:"loading"!==n.readyState)e();else{var t=!1;n.addEventListener("readystatechange",function(){t||(t=!0,e())},!1)}},a=function(e){return t.document.querySelector('link[href$="'+e+'"]')},c=function(e){var t,n,o,a,c,i,u={};if(t=e.sheet,!t)return u;n=t.cssRules?t.cssRules:t.rules;for(var l=0;n.length>l;l++)o=n[l].cssText,a=r+n[l].selectorText,c=o.split(");")[0].match(/US\-ASCII\,([^"']+)/),c&&c[1]&&(i=decodeURIComponent(c[1]),u[a]=i);return u},i=function(e){var t,o,a;o="data-grunticon-embed";for(var c in e)if(a=c.slice(r.length),t=n.querySelectorAll(a+"["+o+"]"),t.length)for(var i=0;t.length>i;i++)t[i].innerHTML=e[c],t[i].style.backgroundImage="none",t[i].removeAttribute(o);return t},u=function(t){"svg"===e.method&&o(function(){i(c(a(e.href))),"function"==typeof t&&t()})};e.embedIcons=i,e.getCSS=a,e.getIcons=c,e.ready=o,e.svgLoadedCallback=u,e.embedSVG=u})(grunticon,this);
+			
+			grunticon(["https://www.lib.utk.edu/template/2016/library/css/icons.data.svg.css", "https://www.lib.utk.edu/template/2016/library/css/icons.data.png.css", "library/css/icons.fallback.css"]);
+		</script>
+		<noscript><link href="https://www.lib.utk.edu/template/2016/library/css/icons.fallback.css" rel="stylesheet"></noscript>
+
+    <link rel='stylesheet' id='utthehill-style-css'  href='https://www.lib.utk.edu/template/2016/style.css?ver=2015-05-20' type='text/css' media='all' />
+    <!--[if lt IE 9]>
+    <link rel='stylesheet' id='utthehill-ie-css'  href='https://www.lib.utk.edu/template/2016/library/css/ie.css?ver=2015-05-20' type='text/css' media='all' />
+    <![endif]-->
+    <script type='text/javascript' src='//code.jquery.com/jquery-1.11.2.min.js'></script>
+
+
+
+
+<!--[if lt IE 9]>
+	  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+
+
+<body class="dropnav page-template-template-nosidebar webapp">
+
+<!-- The UT Header begins here. Do not edit in this area -->
+
+  <div id="orange-bar"></div>
+
+  <div id="page" class="hfeed site row-offcanvas">
+    <div id="main" class="site-main">
+		  <a class="sr-only sr-only-focusable" href="#content" title="Skip to content">Skip to content</a>
+
+  		<header id="masthead-webapp" class="webapp-site-header" role="banner">
+        <h3 class="killer-logo"><a href="http://www.utk.edu">The University of Tennessee, Knoxville</a></h3>
+        <h2 class="sr-only"><a href="https://www.lib.utk.edu" rel="home">University Libraries</a></h2>
+        
+        <div id="sitetitle-webapp">
+          <h2 class="department"><a href="https://www.lib.utk.edu" title="University Libraries" rel="home">University Libraries</a>
+                <!--       <small><a href="http://www.utk.edu">Parent Unit</a></small> --></h2>
+        </div>
+  		</header><!-- #masthead -->
+  		<!-- #Find Box -->
+
+
+      
+	<div id="primary" class="web-app-content-area">
+		<div id="content" class="site-content site-content wide" role="main">
+
+				<article class="hentry">
+
+                <!-- And here begins the Content area. This is where you place your content -->
+
+								<div class="entry-content reg">
+                  <header class="entry-header"><h1 class="entry-title">Front Page</h1></header>
+									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+                  <p>Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium. Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
+                  <p>Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium. Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
+                  <p>Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium. Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
+
+                <!-- And here are a couple of helpers to get you started in page layout -->
+                <!-- Whenever you need to begin a fresh 'new row' of content. Add br with the class of clear  -->
+                <br class="clear">
+                  <div class="half">
+                    <div class="box-light">
+                      <p>Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre.</p>
+                    </div>
+                  </div>
+                  <div class="half">
+                    <div class="box-light">
+                      <p>Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre.</p>
+                    </div>
+                  </div>
+								</div><!-- .entry-content -->
+                <!-- And here ends the Content area. -->
+				</article><!-- #post -->
+
+		</div>
+                <!-- If you would like to have an 'expanded footer' (four columns of supplementary content) you should place it here.  -->
+                <!-- For a sample of the Expanded footer's HTML, check the `/library/partials/` directory.  -->
+	</div><!-- #primary -->
+</div><!-- .main-content -->
+
+<!--  begin footer include -->
+<?php include("https://www.lib.utk.edu/template/2016/includes-utlibraries/footer.php"); ?>
+<!--  end footer include -->
+
+  <div id="system-indicia">
+    <p>The flagship campus of <a href="http://tennessee.edu">the University of Tennessee System</a> and partner in <a href="http://www.tntransferpathway.org/">the Tennessee Transfer Pathway</a>.</p>
+  </div>
+
+</div><!-- #page -->
+
+
+<script type='text/javascript' src='library/js/min/utk-min.js?ver=2015-05-20'></script>
+
+		</body>
+</html>

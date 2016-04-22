f8cfb97a9c4ac94bd42709c84e98768f0cdb82c0
Dan Greene <dgreene@utk.edu>
2016-04-22 13:54:52 -0400

    Libraries left navigation include

---
+++ includes-utlibraries/libnav.php
@@ -0,0 +1,150 @@
+<!-- Begin Search and Find -->
+              <li class="top-menu-item">
+              <button id="drop2" class="list-item-button" aria-haspopup="true" role="button" tabindex="3">Search and Find <i class="icon-fa-chevron-right pull-right"></i></button>
+                <div class="megamenu-sub" id="menu-one"  aria-labelledby="drop2" aria-expanded="false">
+                  <button class="menu-back btn"  data-toggle="dropdown"  role="button"><i class="icon-fa-chevron-left"></i> <span class="back">Back</span></button>
+                    <h3>Search and Find </h3>
+                    <div class="inner">
+                    <div class="menu-header">
+                      <ul class="menu">
+                     <li class="menu-item"><a href="https://www.lib.utk.edu/onesearch/">One Search</a></li>
+					 <li class="menu-item"><a href="https://www.lib.utk.edu/databases/">Articles and Databases</a></li>
+					 <li class="menu-item"><a href="https://www.lib.utk.edu/ejournals">E-Journals</a></li>
+					 <li class="menu-item"><a href="https://www.lib.utk.edu/reserves">Course Reserves</a></li>
+					 <li class="menu-item"><a href="http://digital.lib.utk.edu">Digital Collections</a></li>
+					 <li class="menu-item"><a href="http://dlc.lib.utk.edu/spc/search">Special Collections Online</a></li>
+					 <li class="menu-item"><a href="http://libguides.utk.edu/govinfo">Government Information</a></li>
+					 <li class="menu-item"><a href="http://libanswers.utk.edu/faq/73573">UT Dissertations</a></li>
+					 <li class="menu-item"><a href="http://libguides.utk.edu/streaming">Streaming Video</a></li>
+                      </ul>
+                    </div>
+                  </div>
+            </div>
+              </li>
+              <!-- End Find -->
+               <!-- Begin Use the Library -->
+              <li class="top-menu-item">
+                <button id="drop3" class="list-item-button" aria-haspopup="true" role="button" tabindex="4">Use the Library <i class="icon-fa-chevron-right pull-right"></i></button>
+                <div class="megamenu-sub dropdown-menu" id="menu-two"  aria-labelledby="drop3" aria-expanded="false">
+                  <button  class="menu-back btn" data-toggle="dropdown" role="button" ><i class="icon-fa-chevron-left"></i> <span class="back">Back</span></button>
+                  <h3>Use the Library </h3>
+                  <div class="inner">
+                    <div class="menu-header">
+                      <ul class="menu">
+            <li class="menu-item"><a href="https://www.lib.utk.edu/access/borrow-renew/">Borrow Items</a></li>
+            <li class="menu-item"><a href="https://www.lib.utk.edu/access/renewals/renew-items/">Renew Items</a></li>
+            <li class="menu-item"><a href="https://www.lib.utk.edu/request/">Request Items</a></li>
+            <li class="menu-item"><a href="https://www.lib.utk.edu/askusnow/">Research Consultations</a></li>
+            <li class="menu-item"><a href="https://www.lib.utk.edu/scholar/">Scholars’ Collaborative</a></li>
+            <li class="menu-item"><a href="https://www.lib.utk.edu/access/rooms/">Reserve a Room</a></li>
+            <li class="menu-item"><a href="https://www.lib.utk.edu/access/course-reserves">Place Items on Reserve</a></li>
+            <li class="menu-item"><a href="https://www.lib.utk.edu/info/disabilities">Disabilities Services</a></li>
+            <li class="menu-item"><a href="https://www.lib.utk.edu/access/accounts/">My Library Accounts</a></li>
+            
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <!-- End Use the Library -->
+              <!-- Begin Research Support -->
+              <li class="top-menu-item">
+                <button id="drop4" class="list-item-button" aria-haspopup="true" role="button" tabindex="5">Research Support <i class="icon-fa-chevron-right pull-right"></i></button>
+                <div class="megamenu-sub dropdown-menu" id="menu-three"  aria-labelledby="drop4" aria-expanded="false">
+                  <button  class="menu-back btn" data-toggle="dropdown" role="button" ><i class="icon-fa-chevron-left"></i> <span class="back">Back</span></button>
+                  <h3>Research Support </h3>
+                  <div class="inner">
+                    <div class="menu-header">
+                      <ul class="menu">
+					   <li class="menu-item"><a href="https://www.lib.utk.edu/askusnow/subject-librarians/">Subject Librarians</a></li>
+					   <li class="menu-item"><li><a href="http://www.lib.utk.edu/askusnow/appointment/">Research Consultation</a></li>
+					   <li class="menu-item"><li><a href="http://libguides.utk.edu/">Research Guides</a></li>
+					   <li class="menu-item"><li><a href="http://libguides.utk.edu/style/">Citing Sources</a></li>
+					   <li class="menu-item"><a href="http://libguides.utk.edu/citeman/">EndNote | Zotero</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <!-- End Research Support -->
+              <!-- Begin Libraries and Locations -->
+              <li class="top-menu-item">
+                <button id="drop5" class="list-item-button" aria-haspopup="true" role="button" tabindex="6"> Libraries and Locations <i class="icon-fa-chevron-right pull-right"></i></button>
+                <div class="megamenu-sub dropdown-menu" id="menu-four"  aria-labelledby="drop5" aria-expanded="false">
+                  <button  class="menu-back btn" data-toggle="dropdown" role="button" ><i class="icon-fa-chevron-left"></i> <span class="back">Back</span></button>
+                  <h3>Libraries and Locations </h3>
+                  <div class="inner">
+                    <div class="menu-header">
+                      <ul class="menu">
+					<li class="menu-item"><a href="https://www.lib.utk.edu/">Hodges - Main Library</a></li>
+					<li class="menu-item"><a href="https://www.lib.utk.edu/agvet/">Agriculture and Veterinary Medicine Library</a></li>
+					<li class="menu-item"><a href="https://www.lib.utk.edu/music/">Music Library</a></li>
+					<li class="menu-item"><a href="https://www.lib.utk.edu/swln/">Social Work Library - Nashville</a></li>
+					<li class="menu-item"><a href="http://www.lib.utk.edu/special">Special Collections</a></li>
+					<li class="menu-item"><a href="http://www.lib.utk.edu/special/mpa/">Modern Political Archives</a></li>
+					<li class="menu-item"><a href="https://www.lib.utk.edu/request/storage/">Storage and Map Collection</a></li>
+					<li class="menu-item"><a href="http://commons.utk.edu">The Commons</a></li>
+					<li class="menu-item"><a href="https://www.lib.utk.edu/studio/">The Studio</a></li>
+					<li class="menu-item"><a href="http://onestop.utk.edu">One Stop</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <!-- End Locations -->
+              <!-- Begin Get Help -->
+              <li class="top-menu-item">
+                <button id="drop3" class="list-item-button" aria-haspopup="true" role="button" tabindex="7">Get Help <i class="icon-fa-chevron-right pull-right"></i></button>
+                <div class="megamenu-sub dropdown-menu" id="menu-five"  aria-labelledby="drop6" aria-expanded="false">
+                  <button  class="menu-back btn" data-toggle="dropdown" role="button" ><i class="icon-fa-chevron-left"></i> <span class="back">Back</span></button>
+                  <h3>Get Help </h3>
+                  <div class="inner">
+                    <div class="menu-header">
+                      <ul class="menu">
+                       <li class="menu-item"><a href="https://www.lib.utk.edu/askusnow/">AskUsNow</a></li>
+					   <li class="menu-item"><a href="http://libanswers.utk.edu/">Questions and Answers (FAQ)</a></li>
+					   <li class="menu-item menu-item-has-children" aria-haspopup="true" aria-expanded="false"><a href="https://www.lib.utk.edu/info/">Resources For:</a>
+					     <ul class="sub-menu">
+                          <li class="menu-item"><a title="Information for Undergraduates" href="http://www.lib.utk.edu/info/undergrad/">Undergraduates</a></li>
+	                      <li class="menu-item"><a title="Information for Graduate Students" href="http://www.lib.utk.edu/info/grad/">Graduate Students</a></li>
+	                      <li class="menu-item"><a title="Information for Faculty" href="http://www.lib.utk.edu/info/faculty/">Faculty</a></li>
+	                      <li class="menu-item"><a title="Information for Community and Visitors" href="http://www.lib.utk.edu/info/community/">Community</a></li>
+	                      <li class="menu-item"><a title="Disabilities Services" href="http://www.lib.utk.edu/info/disabilities/">Patrons with Disabilities</a></li>
+	
+                          </ul></li>
+					   <li class="menu-item"><a href="http://libguides.utk.edu/tutorials">Tutorials</a></li>
+					   <li class="menu-item"><a href="https://www.lib.utk.edu/instruct/">Request Instruction</a></li>
+					   <li class="menu-item"><a href="https://www.lib.utk.edu/info/tech/">Technology Support</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <!-- End Get Help -->
+                    <!-- Begin About -->
+              <li class="top-menu-item">
+                <button id="drop3" class="list-item-button" aria-haspopup="true" role="button" tabindex="8">About <i class="icon-fa-chevron-right pull-right"></i></button>
+                <div class="megamenu-sub dropdown-menu" id="menu-six"  aria-labelledby="drop7" aria-expanded="false">
+                  <button  class="menu-back btn" data-toggle="dropdown" role="button" ><i class="icon-fa-chevron-left"></i> <span class="back">Back</span></button>
+                  <h3>About </h3>
+                  <div class="inner">
+                    <div class="menu-header">
+                      <ul class="menu">
+                       <li class="menu-item"><a href="https://www.lib.utk.edu/hours">Hours</a></li>
+                       <li class="menu-item"><a href="https://www.lib.utk.edu/directory">Staff Directory</a></li>
+                       <li class="menu-item"><a href="https://www.lib.utk.edu/about/maps/">Maps and Directions</a></li>
+                       <li class="menu-item"><a href="https://www.lib.utk.edu/news/">News and Events</a></li>
+                       <li class="menu-item"><a href="http://www.lib.utk.edu/assessment/statistics/">Reports and Quick Facts</a></li> 
+                       <li class="menu-item"><a href="https://www.lib.utk.edu/about/administration/">Administration</a></li>
+                       <li class="menu-item"><a href="https://www.lib.utk.edu/employment/">Employment</a></li>
+                       <li class="menu-item"><a href="https://www.lib.utk.edu/society/">The Library Society</a></li>
+					   
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <!-- End About -->
+			  <!-- Begin Giving -->
+              <li id="giving"><a href="https://securelb.imodules.com/s/1341/utk/index.aspx?sid=1341&gid=2&pgid=3204&cid=4841&dids=37">Give to the Libraries<i class="icon-fa-gift fa-lg pull-right"></i></a></li>
+              <!-- End Giving -->

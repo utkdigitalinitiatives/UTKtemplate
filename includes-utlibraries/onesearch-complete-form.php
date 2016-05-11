9bd7f8d95ed9b4c68c9e7c5df4f64369c37daa45
Dan Greene <dgreene@utk.edu>
2016-05-11 14:02:51 -0400

    A One Search include

---
+++ includes-utlibraries/onesearch-complete-form.php
@@ -0,0 +1,21 @@
+				   <div class="search-container">
+<img class="alignnone" alt="One Search" src="https://www.lib.utk.edu/template/2015/images/onesearch.png" />
+<p class="search-options"><a href="http://utk-almaprimo.hosted.exlibrisgroup.com/primo_library/libweb/action/search.do?mode=Advanced&ct=AdvancedSearch&vid=01UTK">Advanced Search</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://utk-almaprimo.hosted.exlibrisgroup.com/primo_library/libweb/action/search.do?fn=showBrowse&mode=BrowseSearch&vid=01UTK">Browse UT Collections</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://utk-almaprimo.hosted.exlibrisgroup.com/pds?func=load-login&institute=01UTK&calling_system=primo&lang=eng&url=http://utk-almaprimo.hosted.exlibrisgroup.com:80/primo_library/libweb/action/login.do?targetURL=http://utk-almaprimo.hosted.exlibrisgroup.com/primo_library/libweb/action/search.do?menuitem=0&fromTop=true&fromPreferences=false&fromEshelf=false&vid=01UTK">Off-campus users: Sign in</a></p>
+
+<div class="search">
+	<form accept-charset="iso-8859-1" action="/onesearch.php" method="post"><input class="searchfield" title="search" name="box" type="text" onfocus="if(this.value == 'Search for books, articles, media, digital collections . . .') { this.value = ''; }" value="Search for books, articles, media, digital collections . . ." width="100%" />
+<p class="search-selector">Limit search to: <input checked="checked" name="searchtype" type="radio" value="onesearch" hidden="yes" />
+<input name="searchtype" type="radio" value="utcollections" /> UT Collections
+<input name="searchtype" type="radio" value="coursereserves" /> Course Reserves</p>
+
+</form>
+<p class="smaller"><a href="http://www.lib.utk.edu/databases/">Articles and Databases</a>
+|
+<a href="https://browzine.com/libraries/203/subjects">E-journals by Subject </a>
+|
+<a href="http://www.lib.utk.edu/ejournals">E-journals by Title </a>
+|
+<a href="http://libguides.utk.edu/databases/490">Google Scholar</a></p>
+
+</div>			    
+					 </div>

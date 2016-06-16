403096548c4659382f2f8dc246285743162dc84a
Dan Greene <dgreene@utk.edu>
2016-06-16 15:48:07 -0400

    Made JS and CSS references absolute to www so the templates can be used anywhere but refer the www files for centralized updating

--- index.php
+++ index.php
@@ -16,9 +16,9 @@
 	<meta name="viewport" content="width=device-width">
 	<title>University Libraries | The University of Tennessee, Knoxville</title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="shortcut icon" href="/images/interface/icon-114x114.png">
-<link rel="shortcut icon" href="/images/interface/favicon.gif">
-<link rel="shortcut icon" href="/images/interface/favicon.ico">
+<link rel="shortcut icon" href="https://www.lib.utk.edu/template/2016/images/interface/icon-114x114.png">
+<link rel="shortcut icon" href="https://www.lib.utk.edu/template/2016/images/interface/favicon.gif">
+<link rel="shortcut icon" href="https://www.lib.utk.edu/template/2016/images/interface/favicon.ico">
 
 
 
@@ -29,17 +29,17 @@
 
       (function(e){function t(t,n,r,o){"use strict";function a(){for(var e,n=0;u.length>n;n++)u[n].href&&u[n].href.indexOf(t)>-1&&(e=!0);e?i.media=r||"all":setTimeout(a)}var i=e.document.createElement("link"),l=n||e.document.getElementsByTagName("script")[0],u=e.document.styleSheets;return i.rel="stylesheet",i.href=t,i.media="only x",i.onload=o||null,l.parentNode.insertBefore(i,l),a(),i}var n=function(r,o){"use strict";if(r&&3===r.length){var a=e.navigator,i=e.Image,l=!(!document.createElementNS||!document.createElementNS("http://www.w3.org/2000/svg","svg").createSVGRect||!document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image","1.1")||e.opera&&-1===a.userAgent.indexOf("Chrome")||-1!==a.userAgent.indexOf("Series40")),u=new i;u.onerror=function(){n.method="png",n.href=r[2],t(r[2])},u.onload=function(){var e=1===u.width&&1===u.height,a=r[e&&l?0:e?1:2];n.method=e&&l?"svg":e?"datapng":"png",n.href=a,t(a,null,null,o)},u.src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==",document.documentElement.className+=" grunticon"}};n.loadCSS=t,e.grunticon=n})(this);(function(e,t){"use strict";var n=t.document,r="grunticon:",o=function(e){if(n.attachEvent?"complete"===n.readyState:"loading"!==n.readyState)e();else{var t=!1;n.addEventListener("readystatechange",function(){t||(t=!0,e())},!1)}},a=function(e){return t.document.querySelector('link[href$="'+e+'"]')},c=function(e){var t,n,o,a,c,i,u={};if(t=e.sheet,!t)return u;n=t.cssRules?t.cssRules:t.rules;for(var l=0;n.length>l;l++)o=n[l].cssText,a=r+n[l].selectorText,c=o.split(");")[0].match(/US\-ASCII\,([^"']+)/),c&&c[1]&&(i=decodeURIComponent(c[1]),u[a]=i);return u},i=function(e){var t,o,a;o="data-grunticon-embed";for(var c in e)if(a=c.slice(r.length),t=n.querySelectorAll(a+"["+o+"]"),t.length)for(var i=0;t.length>i;i++)t[i].innerHTML=e[c],t[i].style.backgroundImage="none",t[i].removeAttribute(o);return t},u=function(t){"svg"===e.method&&o(function(){i(c(a(e.href))),"function"==typeof t&&t()})};e.embedIcons=i,e.getCSS=a,e.getIcons=c,e.ready=o,e.svgLoadedCallback=u,e.embedSVG=u})(grunticon,this);
 			
-			grunticon(["library/css/icons.data.svg.css", "library/css/icons.data.png.css", "library/css/icons.fallback.css"]);
+			grunticon(["https://www.lib.utk.edu/template/2016/library/css/icons.data.svg.css", "library/css/icons.data.png.css", "library/css/icons.fallback.css"]);
 		</script>
-		<noscript><link href="library/css/icons.fallback.css" rel="stylesheet"></noscript>
+		<noscript><link href="https://www.lib.utk.edu/template/2016/library/css/icons.fallback.css" rel="stylesheet"></noscript>
 
 
 
-<link rel='stylesheet' id='utthehill-style-css'  href='style.css?ver=2015-05-20' type='text/css' media='all' />
-<!--[if lt IE 9]>
-<link rel='stylesheet' id='utthehill-ie-css'  href='library/css/ie.css?ver=2015-05-20' type='text/css' media='all' />
-<![endif]-->
-<script type='text/javascript' src='//code.jquery.com/jquery-1.11.2.min.js'></script>
+<link rel='stylesheet' id='utthehill-style-css'  href='https://www.lib.utk.edu/template/2016/style.css?ver=2015-05-20' type='text/css' media='all' />
+    <!--[if lt IE 9]>
+    <link rel='stylesheet' id='utthehill-ie-css'  href='https://www.lib.utk.edu/template/2016/library/css/ie.css?ver=2015-05-20' type='text/css' media='all' />
+    <![endif]-->
+    <script type='text/javascript' src='//code.jquery.com/jquery-1.11.2.min.js'></script>
 
 
 
@@ -47,6 +47,11 @@
 <!--[if lt IE 9]>
 	  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
+<style>
+	.libchat_online, .libchat_offline {
+	text-align: left !important;
+	}
+	</style> 
 </head>
 <body class="dropnav">
 
@@ -65,9 +70,8 @@
     <div id="sidebar">
   		<header id="masthead" class="site-header" role="banner">
               <h3 class="killer-logo"><a href="http://www.utk.edu">The University of Tennessee, Knoxville</a></h3>
-              <h2 class="sr-only"><a href="/" rel="home">UNIT NAME</a></h2>
               <button type="button" class="toggle close findpage">
-                <span class="sr-only">Toggle navigation</span> <i class="icon-fa-find"></i><br>FIND <br> PAGE              </button>
+                <span class="sr-only">Toggle navigation</span> <i class="icon-fa-find"></i><br>NAV <br> MENU              </button>
   		</header><!-- #masthead -->
   		<!-- #Find Box -->
 
@@ -104,8 +108,7 @@
       
 	<div id="primary" class="content-area">
 		<div id="sitetitle" class="site-header">
-      <h2 class="department"><a href="/" title="Your UT Unit Name" rel="home">Your UT Unit Name</a>
-<!--       <small><a href="http://www.utk.edu">Parent Unit</a></small> --></h2> 
+      <h2 class="department"><a href="https://lib.utk.edu" title="University Libraries" rel="home">University Libraries</a>
 		</div>
 
 
@@ -118,7 +121,7 @@
                 <!-- And here begins the Content area. This is where you place your content -->
 
 								<div class="entry-content reg">
-                  <header class="entry-header"><h1 class="entry-title">Front Page</h1></header>
+                  <header class="entry-header"><h1 class="entry-title">Title</h1></header>
 									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
                   <p>Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium. Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
                   
@@ -138,7 +141,7 @@
                     </div>
                   </div>
 								</div><!-- .entry-content -->
-                <!-- And here begins the Content area. This is where you place your content -->
+               
 						
 					<footer class="entry-meta">
 
@@ -147,14 +150,6 @@
 
 			
 		</div>
-
-
-
-                <!-- If you would like to have an 'expanded footer' (four columns of supplementary content) you should place it here.  -->
-                <!-- For a sample of the Expanded footer's HTML, check the `/library/partials/` directory.  -->
-
-
-
 
 	</div><!-- #primary -->
 </div><!-- .main-content -->
@@ -172,7 +167,7 @@
 </div><!-- #page -->
 
 
-<script type='text/javascript' src='library/js/min/utk-min.js?ver=2015-05-20'></script>
+<script type='text/javascript' src='https://www.lib.utk.edu/template/2016/library/js/min/utk-min.js?ver=2015-05-20'></script>
 
 		</body>
 </html>

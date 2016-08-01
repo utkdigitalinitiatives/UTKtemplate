f4b6394fa48e16591950d381f70e49ca68b40955
Dan Greene <dgreene@utk.edu>
2016-08-01 13:41:39 -0400

    One Search search box include

---
+++ includes-utlibraries/search-box.php
@@ -0,0 +1,20 @@
+<div class="search-box">
+<div class="search-link-box">
+<div class="search-link">
+<a href="https://utk-almaprimo.hosted.exlibrisgroup.com/pds?func=load-login&institute=01UTK&calling_system=primo&lang=eng&url=http://utk-almaprimo.hosted.exlibrisgroup.com:80/primo_library/libweb/action/login.do?targetURL=http://utk-almaprimo.hosted.exlibrisgroup.com/primo_library/libweb/action/search.do?menuitem=0&fromTop=true&fromPreferences=false&fromEshelf=false&vid=01UTK">Sign in</a>
+</div>
+|
+<div class="search-link">
+<a href="http://utk-almaprimo.hosted.exlibrisgroup.com/primo_library/libweb/action/search.do?mode=Advanced&ct=AdvancedSearch&vid=01UTK">Advanced Search</a>
+</div>
+|
+<div class="search-link">
+<a href="http://utk-almaprimo.hosted.exlibrisgroup.com/primo_library/libweb/action/search.do?fn=showBrowse&mode=BrowseSearch&vid=01UTK">Browse UT Collections</a>
+</div>
+<form accept-charset="iso-8859-1" action="/onesearch.php" method="post"><input class=" form-control searchfield" title="search" name="box" type="text" onfocus="if(this.value == 'Search for books, articles, media, digital collections . . .') { this.value = ''; }" value="Search for books, articles, media, digital collections . . ." width="100%" />
+	
+<div class="search-options">
+	Limit search to: <input checked="checked" name="searchtype" type="radio" value="onesearch" hidden="yes" /> <input name="searchtype" type="radio" value="utcollections" /> UT Collections <input name="searchtype" type="radio" value="coursereserves" /> Course Reserves
+	</div>
+</form>
+</div>

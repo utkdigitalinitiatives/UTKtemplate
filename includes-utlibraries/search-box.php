ad33556e21e88ce293668864ad815fad4e89f754
Dan Greene <dgreene@utk.edu>
2016-08-02 13:22:38 -0400

    Add missing closing div

--- includes-utlibraries/search-box.php
+++ includes-utlibraries/search-box.php
@@ -11,6 +11,7 @@
 <div class="search-link">
 <a href="http://utk-almaprimo.hosted.exlibrisgroup.com/primo_library/libweb/action/search.do?fn=showBrowse&mode=BrowseSearch&vid=01UTK">Browse UT Collections</a>
 </div>
+</div>
 <form accept-charset="iso-8859-1" action="/onesearch.php" method="post"><input class=" form-control searchfield" title="search" name="box" type="text" onfocus="if(this.value == 'Search for books, articles, media, digital collections . . .') { this.value = ''; }" value="Search for books, articles, media, digital collections . . ." width="100%" />
 	
 <div class="search-options">

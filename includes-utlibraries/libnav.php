9710b39a66a714d850d355de2f41be9ac968bea3
Dan Greene <dgreene@utk.edu>
2016-09-27 13:32:19 -0400

    Correct Ejournals by Title URL

--- includes-utlibraries/libnav.php
+++ includes-utlibraries/libnav.php
@@ -12,7 +12,8 @@
 					 <li class="menu-item menu-item-has-children" aria-haspopup="true" aria-expanded="false"><a href="https://www.lib.utk.edu/finding-ejournals/">E-Journals:</a>
 					     <ul class="sub-menu">
                           <li class="menu-item"><a title="E-Journals by Subject" href="https://www.lib.utk.edu/ejournals/">By Subject</a></li>
-	                      <li class="menu-item"><a title="E-Journals by Title" href="http://utk-almaprimo.hosted.exlibrisgroup.com/primo_library/libweb/action/dlSearch.do?vid=01UTK&institution=01UTK&azSearch=true&fn=almaAzSearch/">By Title</a></li>
+	                      <li class="menu-item">
+	                      <a title="E-Journals by Title" href="http://utk-almaprimo.hosted.exlibrisgroup.com/primo_library/libweb/action/dlSearch.do?vid=01UTK&institution=01UTK&azSearch=true&fn=almaAzSearch">By Title</a></li>
 
 	
                           </ul></li>

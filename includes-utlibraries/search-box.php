746d9b5dcf4110cdb97f88794e17d29e5aa78839
Dan Greene <dgreene@utk.edu>
2017-09-14 12:57:46 -0400

    Remove IE9 conditional CSS, https and URL tweaks

--- includes-utlibraries/search-box.php
+++ includes-utlibraries/search-box.php
@@ -1,15 +1,14 @@
 <div class="search-box">
 <div class="search-link-box">
 <div class="search-link">
-<a href="https://utk-almaprimo.hosted.exlibrisgroup.com/pds?func=load-login&calling_system=primo&institute=01UTK&lang=eng&url=https://utk-almaprimo.hosted.exlibrisgroup.com:443/primo_library/libweb/pdsLogin?targetURL=https://utk-almaprimo.hosted.exlibrisgroup.com/primo-explore/search?vid=01UTK_NEWUI&sortby=rank&lang=en%255FUS&from-new-ui=1&authenticationProfile=BASE_PROFILE">Sign in</a>
+<a href="https://www.lib.utk.edu/onesearch-signin">Sign in</a></div>
+|
+<div class="search-link">
+<a href="https://utk-almaprimo.hosted.exlibrisgroup.com/primo-explore/search?vid=01UTK&mode=advanced" target="_blank">Advanced Search</a>
 </div>
 |
 <div class="search-link">
-<a href="https://utk-almaprimo.hosted.exlibrisgroup.com/primo-explore/search?vid=01UTK_NEWUI&mode=advanced" target="_blank">Advanced Search</a>
-</div>
-|
-<div class="search-link">
-<a href="https://utk-almaprimo.hosted.exlibrisgroup.com/primo-explore/browse?vid=01UTK_NEWUI" target="_blank">Browse UT Collections</a>
+<a href="https://utk-almaprimo.hosted.exlibrisgroup.com/primo-explore/browse?vid=01UTK" target="_blank">Browse UT Collections</a>
 </div>
 </div>
 <form accept-charset="UTF-8" action="/onesearch.php" method="post"><input class="form-control searchfield" title="search" name="box" type="text" onfocus="if(this.value == 'Search for books, articles, media, digital collections . . .') { this.value = ''; }" value="Search for books, articles, media, digital collections . . ." width="100%" />

48b3712793b5738d7d625d0b1b89e0003e3cb9f5
Dan Greene <dgreene@utk.edu>
2017-09-18 14:27:09 -0400

    E- Jornals by Title URL, Sign in  to My Account text change

--- includes-utlibraries/search-box.php
+++ includes-utlibraries/search-box.php
@@ -1,7 +1,7 @@
 <div class="search-box">
 <div class="search-link-box">
 <div class="search-link">
-<a href="https://www.lib.utk.edu/onesearch-signin">Sign in</a></div>
+<a href="https://www.lib.utk.edu/onesearch-signin">My Account</a></div>
 |
 <div class="search-link">
 <a href="https://utk-almaprimo.hosted.exlibrisgroup.com/primo-explore/search?vid=01UTK&mode=advanced" target="_blank">Advanced Search</a>
@@ -14,7 +14,12 @@
 <form accept-charset="UTF-8" action="/onesearch.php" method="post"><input class="form-control searchfield" title="search" name="box" type="text" onfocus="if(this.value == 'Search for books, articles, media, digital collections . . .') { this.value = ''; }" value="Search for books, articles, media, digital collections . . ." width="100%" />
 	
 <div class="search-options">
-	Limit search to: <input checked="checked" name="searchtype" type="radio" value="onesearch" hidden="yes" /> <input name="searchtype" type="radio" value="utcollections" /> UT Collections <input name="searchtype" type="radio" value="coursereserves" /> Course Reserves
+	Limit search to: &nbsp; <input checked="checked" name="searchtype" type="radio" value="onesearch" hidden="yes" /> 
+	<span class="search-option"><input name="searchtype" type="radio" value="utcollections" /> UT Collections</span>
+	<span class="search-option"> <input name="searchtype" type="radio" value="coursereserves" /> Course Reserves</span>
+	<br class="clear" />
 	</div>
-</form>
+	</form>
 </div>
+<br class="clear" />
+<br class="clear" />
